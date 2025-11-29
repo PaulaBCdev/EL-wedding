788c3f10e1ff3addc046963ae6a3e117cab93c9e
Paula Barrionuevo <paula.barrionuevo.00@gmail.com>
2025-11-29 13:02:52 +0100

    PUT endpoint

--- src/app/api/guests/[id]/route.ts
+++ src/app/api/guests/[id]/route.ts
@@ -1,3 +1,4 @@
+import { UpdateGuestDTO } from "@/src/app/dto/update-guest.dto";
 import dbConnect from "@/src/lib/dbConnect";
 import Guest from "@/src/models/Guest";
 import { NextRequest, NextResponse } from "next/server";
@@ -19,7 +20,40 @@ export async function DELETE(
     return NextResponse.json({ message: "Guest removed from list" });
   } catch (error) {
     return NextResponse.json(
-      { message: error instanceof Error ? error.message : "Unknown error" },
+      {
+        message:
+          error instanceof Error ? error.message : "Error deleting guest",
+      },
+      { status: 400 }
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+
+    await dbConnect();
+
+    let guest = Guest.findById(id);
+
+    if (!guest) {
+      return NextResponse.json({ message: "Guest not found" }, { status: 404 });
+    }
+
+    const updatedData: UpdateGuestDTO = await req.json();
+    guest = await Guest.findByIdAndUpdate(id, updatedData, { new: true });
+
+    return NextResponse.json(guest);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message:
+          error instanceof Error ? error.message : "Error updating guest",
+      },
       { status: 400 }
     );
   }
---
+++ src/app/dto/update-guest.dto.ts
@@ -0,0 +1,9 @@
+export type UpdateGuestDTO = {
+  name?: string;
+  surname?: string;
+  contact?: number;
+  attending?: boolean;
+  children?: number;
+  allergies?: string;
+  notes?: string;
+};
